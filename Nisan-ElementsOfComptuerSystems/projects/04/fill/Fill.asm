8cdc0855ada89a8ca09599856c6f6294e75a1376
David Lisuk <dlisuk@gmail.com>
2012-08-22 02:08:38 -0700

    Wrote initial screen filler, will fill screen with black if a key is pressed, clear otherwise

--- Nisan-ElementsOfComptuerSystems/projects/04/fill/Fill.asm
+++ Nisan-ElementsOfComptuerSystems/projects/04/fill/Fill.asm
@@ -9,3 +9,59 @@
 // the screen should be cleared.
 
 // Put your code here.
+
+@131072
+D=A
+@SCREEN
+D=D+A
+@SCREEN_MAX
+M=D
+
+(TOP)
+// If KBD!=0 we should choose black, else choose white
+@KBD
+D=M
+@COLOR_BLACK
+D;JNE
+
+(COLOR_WHITE)
+//Populate R1 with 0 for white, then go to fill
+@R1
+M=0
+@FILL
+0;JMP
+
+(COLOR_BLACK)
+//Populate R1 with 1 for black
+@R1
+M=0
+M=M-1
+
+(FILL)
+@SCREEN
+D=A
+@R2
+M=D
+
+(FILL_LOOP)
+	//Get color
+	@R1
+	D=M
+	
+	@R2
+	//Set color
+	A=M
+	M=D
+	@R2
+	M=M+1
+
+	//If R2 is after the screen buffer, goto top
+	D=A
+	@SCREEN_MAX
+	D=D-A
+	@TOP
+	D;JGE
+
+	//Loop back
+	@FILL_LOOP
+	0;JMP
