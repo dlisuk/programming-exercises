4150e22ecd621bb140eae553530121e026e61fce
David Lisuk <dlisuk@gmail.com>
2012-08-22 01:27:13 -0700

    Implimented basic multiplier algorithm for positive numbers

--- Nisan-ElementsOfComptuerSystems/projects/04/mult/Mult.asm
+++ Nisan-ElementsOfComptuerSystems/projects/04/mult/Mult.asm
@@ -4,6 +4,40 @@
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Copy R0 into X
+@R0
+D=M
+@X
+M=D
+
+//Clear R2
+@R2
+M=0
+
+// Loop until X<=0
+(LOOP)
+	//If X < 0 jump to END
+	@X
+	D=M
+	@END
+	D;JLE
+
+	//Add R1 to R2
+	@R1
+	D=M
+	@R2
+	M=M+D
+
+	//Decrement X
+	@X
+	M=M-1
+
+	//Loop back
+	@LOOP
+	0;JMP
+(END)
+//Done, now loop for ever
+@END
+0;JMP 
